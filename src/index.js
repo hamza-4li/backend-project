1fca2e07823b5b42eb59f47e988383d21ece9098
Hamza-4li <sy.haza.4li@gmail.com>
2024-08-07 22:54:29 +0500

    add custom api and error handling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import  dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import connectDB from './db/database.js';
 
-dotenv.config({path: './env'})
+dotenv.config({ path: './env' });
 
 connectDB()
-
-
-
-
-
-
-
-
+  .then(() => {
+    app.listen.process.env.PORT || 8000,
+      () => {
+        console.log(`Server is listening at port: ${process.env.PORT}`);
+      };
+  })
+  .catch((err) => {
+    console.log('MongoDB conneection failed !!!', err);
+  });
 
 /*
 import express from 'express';
